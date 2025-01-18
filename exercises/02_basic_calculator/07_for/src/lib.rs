814b7f18e109606d11058bdd4ffd8c99617e8222
Austin Kaylor <AustinJKaylor@eaton.com>
2025-01-18 18:13:18 -0500

    02_basic_calculator - 07_for - completed

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,12 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut result = 1;
+
+    for counter in 1..=n {
+        result *= counter;
+    }
+    
+    result
 }
 
 #[cfg(test)]
