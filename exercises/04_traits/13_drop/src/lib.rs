10928d5d3629d2c126f1a93f4166f6285cf4437d
Austin Kaylor <AustinJKaylor@eaton.com>
2025-01-19 22:02:08 -0500

    04_traits - 13_drop - completed

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,30 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+pub struct DropBomb {
+    pub should_panic: bool
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if self.should_panic == true { 
+            panic!("The bomb was not defused!")
+        }
+    }
+}
+
+impl DropBomb {
+    pub fn new() -> DropBomb{
+        DropBomb {
+            should_panic: true
+        }
+    }
+    
+    pub fn defuse(&mut self) {
+        self.should_panic = false;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
